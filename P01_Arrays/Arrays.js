dda49f802de60934449fc2a0e018da8ab31ccd73
Anshul Sharma <anshulsharma.code@gmail.com>
2026-09-23 15:44:23 +0530

    arrays is JS

--- P01_Arrays/Arrays.js
+++ P01_Arrays/Arrays.js
@@ -15,8 +15,13 @@
 //      h. slice(start, end) --> Returns a copy of a section of an array. For both start and end, a negative index can be used to indicate an offset from the end of the array. For example, -2 refers to the second to last element of the array.
 //      i. splice(start, end) --> Removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements.
 //      j. splice(start, end, elements to insert in place of deleted elements) --> Removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements.
-
-
+//      k. obj1.concat(obj2) --> Combines two or more arrays. This method returns a new array without modifying any existing arrays
+//      l. spread operator -- [...obj1, ...obj2] --> spreading each element and combining in one single array and returns that array.
+//      m. flat(depth) --> Returns a new array with all sub-array elements concatenated into it recursively up to the specified depth. 
+//      n. flat(Infinity) --> Returns a new array with all sub-array elements concatenated into it recursively up to infinite depth. 
+//      o. Array.from(object) --> Creates an array from an iterable object.
+//      p. Array.of(obj1, obj, ...) --> Returns a new array from a set of elements.
+//      q. 
 
 
 
@@ -56,4 +61,32 @@ const myArr = [1, 2, 3, 4, 5];
 // console.log(myArr.slice(1,3))
 // console.log(myArr.splice(1,3, 55))
 // console.log("A", myArr)
+
+// ==================================================== Part - 2 ====================================================
+
+const mv_heros = ["Spider Man", "Thor", "Hulk", "Iron Man"]
+const dc_heros = ["Superman", "Batman", "Flash"]
+
+// Merging two arrays
+// mv_heros.push(dc_heros) // will treat dc_heros as a single value and will append that array in last of mv_arrays
+// console.log(mv_heros)
+
+// const newArr = mv_heros.concat(dc_heros);
+// console.log(newArr)
+
+// const newArr = [...mv_heros , ...dc_heros]
+// console.log(newArr)
+
+// const anothetArr = [1, 2, 3, [4, 5, 6], [7, 8, [9, [10, [11]]]]]
+// console.log(anothetArr.flat(Infinity))
+
+// console.log(Array.isArray("Ansuhl"))
+// console.log(Array.from("Anshul"))
+
+// let a = [10, 11, 12]
+// let b = 20
+// let c = 30
+// let d = 40
+
+// console.log(Array.of(a, b, c, d))
 
